f82ec6abd850a935d1d76f7f769e228af9d5ac72
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-05-25 06:22:15 -0500

    despliegue app

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,29 +1,17 @@
-<?php
+<?php    
     class DataBase{
-        public static function connection(){            
-            $hostname = "localhost";
+        public static function connection(){
+            $hostname = "appwebinvdb.mysql.database.azure.com";
             $port = "3306";
             $database = "dbappwebinv";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+            $username = "albeiro_admin";
+            $password = "Earv1234";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/db/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
 	}
-    // class DataBase{
-    //     public static function connection(){
-    //         $options = array(
-    //             PDO::MYSQL_ATTR_SSL_CA => 'assets/db/DigiCertGlobalRootCA.crt.pem'
-    //         );
-    //         $hostname = "dbappwebinv.mysql.database.azure.com";
-    //         $port = "3306";
-    //         $database = "dbappwebinv";
-    //         $username = "admin_albeiro";
-    //         $password = "Earv1234";
-	// 		$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
-	// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// 		return $pdo;
-	// 	}
-	// }
 ?>
